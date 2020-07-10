0e584ac19d3ebca8b70154cf62b3324ba4a214c9
Umair Khan <umairkhan.cis@gmail.com>
2020-07-10 07:29:52 +0400

    add two sum problem

--- src/test/kotlin/com/noorifytech/algorithms/namcc/TwoSumProblemInUnsortedArray.kt
+++ src/test/kotlin/com/noorifytech/algorithms/namcc/TwoSumProblemInUnsortedArray.kt
@@ -4,7 +4,7 @@ import org.junit.Assert
 import org.junit.Test
 import java.util.*
 
-class TwoSumProblem {
+class TwoSumProblemInUnsortedArray {
 
     private fun isPairExistsInUnsortedArray_bruteForce(arr: IntArray, k: Int): Boolean {
         for (i in 0 until arr.size - 1) {
@@ -24,22 +24,6 @@ class TwoSumProblem {
                 return true
             }
             set.add(complement)
-        }
-
-        return false
-    }
-
-    private fun isPairExistsInSortedArray(arr: IntArray, k: Int): Boolean {
-        var start = 0
-        var end = arr.size - 1
-
-        while (end > start) {
-            val sum = arr[start] + arr[end]
-            when {
-                sum > k -> end--
-                sum < k -> start++
-                else -> return true
-            }
         }
 
         return false
@@ -175,7 +159,6 @@ class TwoSumProblem {
         )
     }
 
-    //    ===============================================================
     @Test
     fun isPairExists_optimized_case1() {
         Assert.assertFalse(isPairExistsInUnsortedArray_optimized(intArrayOf(), 0))
@@ -339,214 +322,6 @@ class TwoSumProblem {
                     -30
                 ), 200
             )
-        )
-    }
-
-    //    ===============================================================
-    @Test
-    fun isPairExistsInSortedArray_case1() {
-        val arr = intArrayOf()
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 0))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case2() {
-        val arr = intArrayOf()
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 5))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case3() {
-        val arr = intArrayOf(4)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 4))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case4() {
-        val arr = intArrayOf(4)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 5))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case5() {
-        val arr = intArrayOf(4)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 0))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case6() {
-        val arr = intArrayOf(6, 4)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 6))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case7() {
-        val arr = intArrayOf(6, 4)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 5))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case8() {
-        val arr = intArrayOf(6, 0, 4)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 6))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case9() {
-        val arr = intArrayOf(8, 1, 6, 20, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 6))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case10() {
-        val arr = intArrayOf(8, 1, 6, -6, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 6))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case11() {
-        val arr = intArrayOf(8, 1, 6, 6, 4)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 12))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case12() {
-        val arr = intArrayOf(8, 1, 6, 6, 4)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 12))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case13() {
-        val arr = intArrayOf(3, 3, 3, 4, 2, 4, 2, 5, 1)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 7))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case14() {
-        val arr = intArrayOf(3, 1, 6, 4, 2, -1)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 5))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case15() {
-        val arr = intArrayOf(3, 5, 20, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 5))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case16() {
-        val arr = intArrayOf(8, 1, 6, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 0))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case17() {
-        val arr = intArrayOf(8, 1, 6, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 0))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case18() {
-        val arr = intArrayOf(8, 1, 6, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(isPairExistsInSortedArray(arr, 0))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case19() {
-        val arr = intArrayOf(8, 1, 6, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 7))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case20() {
-        val arr = intArrayOf(8, 1, 6, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertTrue(isPairExistsInSortedArray(arr, 1))
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case21() {
-        val arr = intArrayOf(8, 1, 6, 4, -3)
-        Arrays.sort(arr)
-        Assert.assertFalse(
-            isPairExistsInSortedArray(arr, Math.pow(10.toDouble(), 5.toDouble()).toInt())
-        )
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case22() {
-        val arr = intArrayOf(
-            8,
-            1,
-            6,
-            4,
-            -3,
-            8,
-            1,
-            6,
-            4,
-            1,
-            6,
-            4,
-            -3,
-            8,
-            1,
-            192,
-            -80,
-            -30
-        )
-        Arrays.sort(arr)
-        Assert.assertTrue(
-            isPairExistsInSortedArray(
-                arr, 200
-            )
-        )
-    }
-
-    @Test
-    fun isPairExistsInSortedArray_case23() {
-        val arr = intArrayOf(
-            8,
-            1,
-            6,
-            4,
-            -3,
-            8,
-            1,
-            6,
-            4,
-            1,
-            6,
-            400,
-            600,
-            8,
-            1,
-            192,
-            -80,
-            -30
-        )
-        Arrays.sort(arr)
-        Assert.assertTrue(
-            isPairExistsInSortedArray(arr, 200)
         )
     }
 }
