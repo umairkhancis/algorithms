3f7a819fb8063061abba1fc670a5dc718459c491
Umair Khan <umairkhan.cis@gmail.com>
2020-07-14 21:36:12 +0400

    Improve FindStringIsPalindrome.kt

--- src/test/kotlin/com/noorifytech/algorithms/patterns/recursive/FindStringIsPalindrome.kt
+++ src/test/kotlin/com/noorifytech/algorithms/patterns/recursive/FindStringIsPalindrome.kt
@@ -6,18 +6,15 @@ import org.junit.Test
 class FindStringIsPalindrome {
 
     private fun isPalindrome(str: String): Boolean {
-        if (str.length <= 1) {
-            return true
-        }
-
-        val isLastPalindrome = isPalindrome(str.slice(1 until str.length - 1))
-        return isLastPalindrome && str[0] == str[str.length - 1]
+        if (str.length <= 1) return true
+        if (str[0] != str[str.length - 1]) return false
+        return isPalindrome(str.slice(1 until str.length - 1))
     }
 
     @Test
     fun isPalindrome_case1() {
         // Arrange
-        val str = "12344321"
+        val str = "racecar"
 
         // Act
         val result = isPalindrome(str)
@@ -29,12 +26,48 @@ class FindStringIsPalindrome {
     @Test
     fun isPalindrome_case2() {
         // Arrange
-        val str = "racecar"
+        val str = "abba"
 
         // Act
         val result = isPalindrome(str)
 
         // Assert
         Assert.assertTrue(result)
+    }
+
+    @Test
+    fun isPalindrome_case3() {
+        // Arrange
+        val str = "a"
+
+        // Act
+        val result = isPalindrome(str)
+
+        // Assert
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun isPalindrome_case4() {
+        // Arrange
+        val str = "hello"
+
+        // Act
+        val result = isPalindrome(str)
+
+        // Assert
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun isPalindrome_case5() {
+        // Arrange
+        val str = "ax"
+
+        // Act
+        val result = isPalindrome(str)
+
+        // Assert
+        Assert.assertFalse(result)
     }
 }
