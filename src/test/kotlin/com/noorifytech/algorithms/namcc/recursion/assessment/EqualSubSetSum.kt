76a81b6263e587f2ebcbde118b1762a447c111b0
Umair Khan <umairkhan.cis@gmail.com>
2020-07-26 22:55:07 +0400

    Add problem description.

--- src/test/kotlin/com/noorifytech/algorithms/namcc/recursion/assessment/EqualSubSetSum.kt
+++ src/test/kotlin/com/noorifytech/algorithms/namcc/recursion/assessment/EqualSubSetSum.kt
@@ -3,6 +3,26 @@ package com.noorifytech.algorithms.namcc.recursion.assessment
 import org.junit.Assert
 import org.junit.Test
 
+/**
+ * Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Example 1:
+
+Input: [1, 5, 11, 5]
+
+Output: true
+
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+
+Example 2:
+
+Input: [1, 2, 3, 5]
+
+Output: false
+
+Explanation: The array cannot be partitioned into equal sum subsets.
+ */
 class EqualSubSetSum {
 
     private fun equalSubSetSum(arr: IntArray): Boolean {
